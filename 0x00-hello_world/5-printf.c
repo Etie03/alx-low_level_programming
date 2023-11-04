cb9b7570824db380fac777e2c5828c138ca94409
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-11-04 09:01:20 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a program that uses a printf function to print a line
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
