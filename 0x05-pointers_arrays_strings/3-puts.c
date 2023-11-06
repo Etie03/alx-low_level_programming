63810e456610d8cdd561bd143c829f684a0f3661
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-11-06 06:09:45 -0800

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * _puts - This function prints a string
+ * @str: string that will be printed
+ *
+ */
+
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str++);
+}
+_putchar('\n');
+}
