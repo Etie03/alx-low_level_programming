a30b4fd43fc2feac42e147a4cfd8a9077976e5cc
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-11-06 07:01:50 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts2 -  function prints every other character of a string
+ * @str: The input
+ * Return: print
+ */
+
+void puts2(char *str)
+{
+int lenl = 0;
+int j = 0;
+char *m = str;
+int n;
+
+while (*m != '\0')
+{
+m++;
+lenl++;
+}
+j = lenl - 1;
+for (n = 0; n <= j; n++)
+{
+if (n % 2 == 0)
+{
+_putchar(str[n]);
+}
+}
+_putchar('\n')
+}
