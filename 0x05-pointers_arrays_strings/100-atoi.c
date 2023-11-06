c89b05f66a94c17e49fe9920a212bbf0d54a79d6
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-11-06 10:56:24 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,12 +8,14 @@
  */
 int _atoi(char *s)
 {
-int i = 0;
-int d = 0;
-int n = 0;
-int len = 0;
-int f = 0;
-int digit = 0;
+int i, d, n, len, f, digit;
+
+i = 0;
+d = 0;
+n = 0;
+len = 0;
+f = 0;
+digit = 0;
 
 while (s[len] != '\0')
 len++;
@@ -30,7 +32,7 @@ if (d % 2)
 digit = -digit;
 n = n * 10 + digit;
 f = 1;
-if (i + 1 < len && (s[i + 1] < '0' || s[i + 1] > '9'))
+if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
 f = 0;
 }
@@ -42,3 +44,5 @@ return (0);
 
 return (n);
 }
+
+
