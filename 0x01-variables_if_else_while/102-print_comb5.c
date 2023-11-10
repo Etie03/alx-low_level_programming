3491f7e343a5d464c04551daa5def5c6ed5312dd
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-11-10 03:12:06 -0800

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - a program that prints two two-digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+int j;
+
+for (i = 0; i <= 98; i++)
+{
+for (j = i + 1; j <= 99; j++)
+{
+putchar('0' + (i / 10));
+putchar('0' + (i % 10));
+putchar(' ');
+putchar('0' + (j / 10));
+putchar('0' + (j % 10));
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
