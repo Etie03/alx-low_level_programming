2cc1a9bc406f20daeed87a4cc24bb2197e1d1560
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-11-14 12:35:35 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,16 @@
 
 /**
  * _isupper - a function that checks for uppercase character
- * @c: The character to be checked
+ * @c: the character to be checked
  *
- * Return: return 1 if c is uppercase else 0
+ * Return: return 1 if uppercase or else 0
  */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-
-	return (0);
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+return (0);
 }
