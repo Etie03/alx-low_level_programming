5fa074e82620f6bec0085f70e5d7a11edd31744d
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-11-14 04:37:58 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,40 +10,40 @@ void print_times_table(int n)
 {
 int p, j, r;
 
-if (n >= 0 && n <= 15)
-{
-for (p = 0; p <= n; p++)
-{
-for (j = 0; j <= n; j++)
-{
-r = j * p;
-if (j == 0)
-{
-_putchar(r + '0');
-} else if (r < 10 && j != 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar(r + '0');
-} else if (r >= 10 && r < 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar((r / 10) + '0');
-_putchar((r % 10) + '0');
-} else if (r >= 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((r / 100) + '0');
-_putchar(((r / 10) % 10) + '0');
-_putchar((r % 10) + '0');
-}
-}
-_putchar('\n');
-}
-}
+	if (n >= 0 && n <= 15)
+	{
+		for (p = 0; p <= n; p++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				r = j * p;
+				if (j == 0)
+				{
+					_putchar(r + '0');
+				} else if (r < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(r + '0');
+				} else if (r >= 10 && r < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((r / 10) + '0');
+					_putchar((r % 10) + '0');
+				} else if (r >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((r / 100) + '0');
+					_putchar(((r / 10) % 10) + '0');
+					_putchar((r % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
