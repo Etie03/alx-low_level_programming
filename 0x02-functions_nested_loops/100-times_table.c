1c73f52f9c997b8d59e354e6cfaab1e25ed4b2cc
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-11-14 04:09:58 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,24 +20,21 @@ r = j * p;
 if (j == 0)
 {
 _putchar(r + '0');
-}
-else if (r < 10 && j != 0)
+} else if (r < 10 && j != 0)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(r + '0');
-}
-else if (r >= 10 && r < 100)
+} else if (r >= 10 && r < 100)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar((r / 10) + '0');
 _putchar((r % 10) + '0');
-}
-else if (r >= 100)
+} else if (r >= 100)
 {
 _putchar(',');
 _putchar(' ');
