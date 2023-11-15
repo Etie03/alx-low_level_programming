f827d285a3a5392451c651129897f674954b2239
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-11-15 12:09:42 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _puts_recursion - a function that prints a string
- * @s: string to be printed
- * Return: nothing
+ * _puts_recursion -a function that prints a string
+ * @s: the string to be printed
+ *
+ * Return: void
  */
 
 void _puts_recursion(char *s)
 {
-
-if (s[0] == '\0')
-_putchar('\n');
-else
-{
-_putchar(s[0]);
-s++;
-_puts_recursion(s);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
