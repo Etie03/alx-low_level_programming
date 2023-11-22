9863673f250220a1f77ccbfac36124b05cb1a65f
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-11-22 14:15:13 -0800

    101-strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,50 +1,78 @@
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * count_words - a function that Counts words in a string based on spaces
- * @str: The string to be counted
+ * count_word - a function that Counts words in a string based on spaces
+ * @s: The string to be counted
  *
- * Return: The number of words of the string else 0
+ * Return: The number of words of the string
  */
-int count_words(char *str)
-{
-	int i = 0, n = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
-		if (str[i] == 32)
-			n++;
-	return (n);
+int count_word(char *s)
+{
+int flag, m, p;
+
+flag = 0;
+p = 0;
+
+for (m = 0; s[m] != '\0'; m++)
+{
+if (s[m] == ' ')
+flag = 0;
+else if (flag == 0)
+{
+flag = 1;
+p++;
+}
+}
+
+return (p);
 }
 /**
  * strtow - a function that splits a string into words
  * @str: The string
  *
- * Return: a pointer to an array of strings
+ * Return: a pointer to an array of strings else NULL
  */
 
 char **strtow(char *str)
 {
-	int i = 0, j = 0, k = 0, n = 0;
-	char **res;
+char **matrix, *tmp;
+int i, k = 0, len = 0, words, c = 0, start, end;
 
-	n = count_words(str);
-	res = malloc(n * sizeof(char *));
-	if (res != NULL)
-	{
-		for (i = 0; i < n; i++)
-		{
-			n = 0;
-			while (str[j++] != 32)
-				n++;
-			j++;
-			res[i] = malloc((n + 1) * sizeof(char));
-			if (res[i] != NULL)
-			{
-				for (k = 0; k < n; k++)
-					res[i][k] = str[j - n - 1 + k];
-				res[i][k] = '\0';
-			}
-		}
-	}
-	return (res);
+while (*(str + len))
+len++;
+words = count_word(str);
+if (words == 0)
+return (NULL);
+
+matrix = (char **) malloc(sizeof(char *) * (words + 1));
+if (matrix == NULL)
+return (NULL);
+
+for (i = 0; i <= len; i++)
+{
+if (str[i] == ' ' || str[i] == '\0')
+{
+if (c)
+{
+end = i;
+tmp = (char *) malloc(sizeof(char) * (c + 1));
+if (tmp == NULL)
+return (NULL);
+while (start < end)
+*tmp++ = str[start++];
+*tmp = '\0';
+matrix[k] = tmp - c;
+k++;
+c = 0;
+}
+}
+else if (c++ == 0)
+start = i;
+}
+
+matrix[k] = NULL;
+
+return (matrix);
 }
