bf84aaf2dd95b0af885f23e8594883d76c7d4a47
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-11-23 13:30:55 -0800

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -56,10 +56,11 @@ void errors(void)
  *
  * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, m, carry, digit1, digit2, *result, p = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -70,8 +71,8 @@ int main(int argc, char *argv[])
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (m = 0; m <= len1 + len2; m++)
-		result[m] = 0;
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
@@ -86,14 +87,14 @@ int main(int argc, char *argv[])
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-	for (m = 0; m < len - 1; m++)
+	for (i = 0; i < len - 1; i++)
 	{
-		if (result[m])
-			p = 1;
-		if (p)
-			_putchar(result[m] + '0');
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-	if (!p)
+	if (!a)
 		_putchar('0');
 	_putchar('\n');
 	free(result);
