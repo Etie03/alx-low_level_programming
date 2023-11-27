ca102ac9f2d21e875580bb6468de74c2fd269925
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-11-27 14:58:08 -0800

    4. SUM

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,20 @@
+#ifndef sum_h
+#define sum_h
+
+#define SUM(x, y)
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int total_sum;
+
+	total_sum = SUM(x, y);
+
+	return (0);
+}
+
+#endif
