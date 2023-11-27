332448dac414cd8e8f96e1f987ea73bb2f55746c
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-11-27 15:34:02 -0800

    4. SUM

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,22 +1,6 @@
 #ifndef _sum_h
 #define _sum_h
 
-#include <stdio.hg>
-
-#define SUM(x, y)
-/**
- * main - entry point
- *
- * Return: 0
- */
-
-int main(void)
-{
-	int s;
-
-	s = SUM(x, y);
-	printf("%d\n", s);
-	return (0);
-}
+#define SUM(x, y) ((x) + (y))
 
 #endif
