66ead94e1d936ca849a4553764639ff12e5c5c40
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-11-28 12:13:53 -0800

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - structure for dog
+ * @*name: first member
+ * @age: second member
+ * @*owner: third member
+ */
+
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+
+} my_dog;
+
+#endif
