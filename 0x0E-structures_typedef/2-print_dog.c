16e53ea2bfdcbaee75eca5df753f6a9fa91de49f
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-11-28 15:17:23 -0800

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,13 +11,23 @@
 void print_dog(struct dog *d)
 {
 if (d != NULL)
+{
 if (d->name == '\0')
+{
 printf("Name: (nil)\n");
+}
 else
+{
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d.->ge);
+}
 if (d->owner == NULL)
+{
 printf("Owner: (nil)\n");
+}
 else
+{
 printf("Owner: %s\n", d->owner);
+}
+}
 }
