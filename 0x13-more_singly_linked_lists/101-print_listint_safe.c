f5a35cd0616555a20d1822bae434cbe895002977
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-12-12 15:53:38 -0800

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,7 +1,6 @@
 #include "lists.h"
-
 /**
- * print_listint_safe - function that prints a linked list
+* print_listint_safe - function that prints a linked list
  * @head: pointer to the head
  *
  * Return: number of nodes in the list
@@ -9,30 +8,30 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t m = 0;
-	listint_t *fau = (void *)head, *test = 0;
-	int flag = 0;
+        size_t m = 0;
+        listint_t *fau = (void *)head, *test = 0;
+        int flag = 0;
 
-	while (fau != NULL)
-	{
-		if (flag == 0)
-		{
-			test = fau;
-			printf("[%p] %d\n", (void *)fau, fau->n);
-		}
-		if (flag == 1)
-		{
-			if (test <= fau)
-			{
-				printf("-> [%p] %d\n", (void *)fau, fau->n);
-				return (m);
-			}
-			printf("[%p] %d\n", (void *)fau, fau->n);
-		}
-		m += 1;
-		test = fau;
-		fau = fau->next;
-		flag = 1;
-	}
-	return (m);
+        while (fau != NULL)
+        {
+                if (flag == 0)
+                {
+                        test = fau;
+                        printf("[%p] %d\n", (void *)fau, fau->n);
+                }
+                if (flag == 1)
+                {
+                        if (test <= fau)
+                        {
+                                printf("-> [%p] %d\n", (void *)fau, fau->n);
+                                return (m);
+                        }
+                        printf("[%p] %d\n", (void *)fau, fau->n);
+                }
+                m += 1;
+                test = fau;
+                fau = fau->next;
+                flag = 1;
+        }
+        return (m);
 }
