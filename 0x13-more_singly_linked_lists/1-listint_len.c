2192d91115882378f03940fc036141a97fc57911
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-12-12 11:39:13 -0800

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * listint_len - a function that returns the number of
@@ -10,14 +11,13 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t count = 0;
+        size_t count = 0;
 
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
+        while (h != NULL)
+        {
+                count++;
+                h = h->next;
+        }
 
-	return (count);
+        return (count);
 }
-
