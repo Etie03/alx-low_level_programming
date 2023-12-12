10fd9218be7b0a04f7b7d9c3af8f0a733da54ffd
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-12-12 11:49:19 -0800

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -13,10 +13,13 @@ size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	if (h != NULL)
 	{
+		while (h)
+		{
 		count++;
 		h = h->next;
+		}
 	}
 
 	return (count);
